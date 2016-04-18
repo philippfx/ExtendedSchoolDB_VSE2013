7a4e7e14d2cf602a167bddd0d1cd2e73a70e3af1
Philipp Seifert <philipp.seifert@iguanasell.com>
2016-04-18 10:51:19 +0200

    Added Text 3

--- Forms/Menu.Designer.cs
+++ Forms/Menu.Designer.cs
@@ -33,6 +33,7 @@
             this.ShowCourses = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // ShowStudents
@@ -83,11 +84,21 @@
             this.label2.TabIndex = 4;
             this.label2.Text = "Will be deleted";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(201, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Text 3";
+            // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.ShowCourses);
@@ -107,5 +118,6 @@
         private System.Windows.Forms.Button ShowCourses;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
